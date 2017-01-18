24afaad09f33d5992f20b9b97efc63db35db44bf
Varun Babu Pozhath <varunbabu008@gmail.com>
2017-01-18 17:55:49 +1100

    init commit

---
+++ MyEmployees.Web/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyEmployees.Web.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public String FullName { get; set; }
+        public String Department { get; set; }
+    }
+}
